3fcef475344f3100bc58d8aec92f2977999e861b
avdhoot-1 <avdhoot1@gmail.com>
2021-05-30 20:54:07 +0530

    commit

---
+++ Choose_number.py
@@ -0,0 +1,10 @@
+a=3
+b=15
+c=25
+d=15
+
+if a>d :
+    print ('a', "is the biggest number")
+elif b>d:
+        print ('b',"is the biggest number")
+else : print ('c',"is the biggest number" )
---
+++ input_table.py
@@ -0,0 +1,7 @@
+a=int(input("enter primary number for multiplication? "))
+b=int(input("Multiply the number till? "))
+c=1
+while  c <= b:
+    print (a, "x", c, "=", a*c)
+    c=c+1
+print ( "Kill Switch" )
