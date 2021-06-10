ed8b61d0c27e69350ea54eca9ba7ea5a6840b615
avdhoot-1 <avdhoot1@gmail.com>
2021-06-10 09:53:33 +0530

    commmit

--- draft.py
+++ draft.py
@@ -1,5 +1,5 @@
-a=10000 #int(input("enter current rent?  "))
-b=5 #int(input("enter rate of increase per year  "))
+a=15000 #int(input("enter current rent?  "))
+b=10 #int(input("enter rate of increase per year  "))
 c=50 #int(input("calculate current rent increase till?  "))
 x=1
 y= a*12
@@ -7,7 +7,8 @@ print ("annual rent = ",y)
 
 while x<=c :
  z=(y*(b/100))
- print ("Year", x, "=", y+z)
+ print ("Annual  Rent Year", x, "=", y+z)
+ print ("Monthly Rent Year", x, "=", (y+z)/12)
  x=x+1
  y=y+z
  
--- fracttion_rent.py
+++ fracttion_rent.py
@@ -1,12 +1,14 @@
-a=int(input("enter current rent?  "))
-b=int(input("enter rate of increase per year  "))
-c=int(input("calculate current rent increase till?  "))
+a=15000 #int(input("enter current rent?  "))
+b=10 #int(input("enter rate of increase per year  "))
+c=50 #int(input("calculate current rent increase till?  "))
 x=1
 y= a*12
 print ("annual rent = ",y)
 
 while x<=c :
  z=(y*(b/100))
- print ("Year", x, "=", y+z)
+ print ("Annual  Rent Year", x, "=", y+z)
+ print ("Monthly Rent Year", x, "=", (y+z)/12)
  x=x+1
  y=y+z
+ 
